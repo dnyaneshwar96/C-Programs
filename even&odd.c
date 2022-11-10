cb161e2c6bbe55d52bc950e632fda0ae7d3b661e
dnyaneshwar96 <116817288+dnyaneshwar96@users.noreply.github.com>
2022-11-09 21:12:39 -0800

    EVEN&ODD.C
    A program for even odd user define value.

---
+++ even&odd.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int num;
+    printf("enter the number");
+    scanf("%d",&num);
+    if(num%2==0)
+    {
+        printf("number is even");
+
+    }
+    else
+    {
+        printf("numbr is odd");
+    }
+    return 0;
+}
